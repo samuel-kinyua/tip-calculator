60b61c8173f4a4f061e7e44c1731308903ba94a9
samuel-kinyua <samuelkinyua722@gmail.com>
2023-02-24 17:57:09 +0300

    added the reset feature that allows users to reset the tip calculator using js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const billInput = document.getElementById('bill');
 const peopleInput = document.getElementById('NumPeople');
 const tipDisplay = document.getElementById('tipNums');
 const totalDisplay = document.getElementById('totalNums');
+const custominput = document.getElementById('custom')
 
 function calculateTip(tipPercentage){
     const bill = parseFloat(billInput.value);
     const people = parseFloat(peopleInput.value);
+   
 
     if (isNaN(bill) || isNaN(people)){
         tipDisplay.textContent = '$0.00';
@@ -34,18 +36,27 @@ document.getElementById('three').addEventListener('click', function(){
     calculateTip(15);
 });
 
-document.getElmentById('four').addEventListener('click', function(){
+document.getElementById('four').addEventListener('click', function(){
     calculateTip(25);
 });
 
-document.getElmentById('five').addEventListener('click', function(){
+document.getElementById('five').addEventListener('click', function(){
     calculateTip(50);
 });
 
 document.getElementById('custom').addEventListener('input', function(){
     const customTip = parseFloat(this.value);
 
-    if(!isNaN(customtip)){
+    if(!isNaN(customTip)){
         calculateTip(customTip);
     }
+});
+
+document.getElementById('reset').addEventListener('click', function() {
+    billInput.value = '';
+    peopleInput.value = '';
+    custominput.value = '';
+    tipDisplay.textContent = '$0.00';
+    totalDisplay.textContent = '$0.00';
+
 });
